ae9e6e13a02deaa9079939ef07588573591bbb20
muhancioglu <muhancioglu@gmail.com>
2022-09-24 05:34:40 +0300

    portfolio session 2 - 08/24/2022 Serdar hodja

---
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/Flask_If_for_structure/sample.py
@@ -0,0 +1,13 @@
+# A = 50
+
+# if A > 30 :
+    # print("bigger than 30")
+# elif 30 > A > 20:
+    # print("between 20 - 30")
+# else :
+    # print("smaller than 20")   
+    # 
+    # 
+    # 
+for value in ["corolla", "honda", "merso"]:
+    print(f'{value}') 
